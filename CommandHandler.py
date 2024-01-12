065f350ac7cb1850c8a66ab701317decd33c6ee1
David Lofton <davey06lego@gmail.com>
2024-01-11 18:19:12 -0600

    users command prototyped

--- CommandHandler.py
+++ CommandHandler.py
@@ -1,10 +1,18 @@
 primative = {"users", "entries", "help"}
 twoStage = {"void", "new"}
 
-def parse(rawInput):
-    if rawInput in primative:
-        
-    elif rawInput in twoStage:
+def parse(raw_input):
+    command_name, *arguments = raw_input.split()
 
+    if command_name in globals() and callable(globals()[command_name]):
+        stack = globals()[command_name]
+        if arguments:
+            stack(arguments[0])
+        else:
+            stack()
     else:
         return "Command not found, try again or type \"help\"."
+    
+
+def users():
+    print("WAHOOO")
