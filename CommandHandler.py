86fdc0989622071cf78c71acadefa3be9de1b113
David Lofton <davey06lego@gmail.com>
2024-01-11 21:23:50 -0600

    USER SELECTION FINISHED YAAAAAAAAAY

--- CommandHandler.py
+++ CommandHandler.py
@@ -1,18 +1,73 @@
-primative = {"users", "entries", "help"}
-twoStage = {"void", "new"}
+import json
+import os
 
-def parse(raw_input):
-    command_name, *arguments = raw_input.split()
+
+
+def screen_clear(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def parse(raw_input, list=[]):
+    if raw_input in list:
+        for i in list:
+            if raw_input == i:
+                return i
+    
+    try: 
+        command_name, *arguments = raw_input.split()
+    except:
+        print("Command empty, try again or type \"help\".")
+        parse(input(), list=list)
 
     if command_name in globals() and callable(globals()[command_name]):
         stack = globals()[command_name]
         if arguments:
             stack(arguments[0])
+            return
         else:
             stack()
+            return
     else:
-        return "Command not found, try again or type \"help\"."
+        print("Command not found, try again or type \"help\".")
+        parse(input(), list=list)
     
 
+
 def users():
-    print("WAHOOO")
+    with open("./data/users.json", "r") as file:
+        users_data = json.load(file)
+
+    user_list = []
+    user_string = ""
+    user_selected = ""
+    for i in users_data["users"].keys():
+        user_list.append(i)
+        if users_data["users"][i] == True:
+            user_string += "%s(selected)\n" % i
+            user_selected = i
+        else:
+            user_string += i + "\n"
+    
+    screen_clear()
+
+    print("Select a user: \n")
+    print(user_string)
+    print("Type \"back\" to return to the homescreen")
+
+    print(user_list)
+
+    requested_user = parse(input(), list=user_list)
+
+    if requested_user in user_list:
+        
+        print("MATCHED")
+        users_data["users"][user_selected] = False
+        users_data["users"][requested_user] = True
+        with open("./data/users.json", "w") as file:
+            json.dump(users_data, file, indent=2)
+            print("DUMPED")
+    else:
+        return
+    
+    screen_clear()
+    print("%s has been selected" % requested_user)
+    
--- Monster.py
+++ Monster.py
@@ -1,9 +1,9 @@
-import os
-import json
-
 import CommandHandler
 
 commandList = {"new", "entries", "users", "void"}
+
+def homescreen():
+    print("Greetings, \nWelcome to the M.O.N.S.T.E.R. \n\nTo start a new entry type \"new entry\" \nType \"help\" to show a list of commands")
 
 #Stores username
 def newUser():
@@ -12,17 +12,16 @@ def newUser():
 
 
         if input("\n%s is correct? (y/n)" % (user)) in {"yes", "y"}:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            CommandHandler.screen_clear()
             return user
+
+
 
 print("Initializing Mycological Observational Navigation System and Tracker for Enviromental Research...")
 
 print("M.O.N.S.T.E.R is ready!")
-
-
-
-print("Greetings, \nWelcome to the M.O.N.S.T.E.R. \n\nTo start a new entry type \"new entry\" \nType \"help\" to show a list of commands")
+homescreen()
 
 CommandHandler.parse(input())
 
-
+print("MONSTER done")
